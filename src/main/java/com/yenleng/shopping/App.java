2747b8ba3d0064021258a4cdb02a0cf1ef02cd3e
Yenleng <yenlengggggg@gmail.com>
2023-04-17 23:30:20 +0800

    explanation for features

--- src/main/java/com/yenleng/shopping/App.java
+++ src/main/java/com/yenleng/shopping/App.java
@@ -24,22 +24,22 @@ public class App
   
         String input = "";
         while (!"stop".equals(input)){
-            input = cons.readLine(">");
+            input = cons.readLine(">"); //puts > at user input
 
             if (input.equals("list")){
-                if (shoppingList.size() == 0){
+                if (shoppingList.size() == 0){ //cart is empty if size is 0
                     System.out.println("Cart is empty");
                 }else{
-                    for (int i = 1; i <= shoppingList.size(); i++){
-                        System.out.println(i + ". " + shoppingList.get(i - 1));
+                    for (int i = 1; i <= shoppingList.size(); i++){ //loops through list size to print 1. 2. 3.
+                        System.out.println(i + ". " + shoppingList.get(i - 1));// -1 from get as index starts from zero
                     }
                 }
             } else if (input.startsWith("add")) {
                 
-                String cmdValue = input.substring(3);
-                String[] items = cmdValue.split(","); //pear ,orange, apple
+                String cmdValue = input.substring(3); //takes everything after 3
+                String[] items = cmdValue.split(","); //splits by comma. pear ,orange, apple
                 for (String item : items) {
-                    item = item.trim();
+                    item = item.trim(); //trims away any empty space if present
                     if (set.contains(item)){
                         System.out.println("You have " + item + " in your cart");
                     } else {
@@ -50,7 +50,7 @@ public class App
                 }
             } else if (input.startsWith("delete")) {
                 String cmdValueStr = input.substring(6);//takes everything after 6
-                int cmdValueInt = Integer.parseInt(cmdValueStr) - 1;
+                int cmdValueInt = Integer.parseInt(cmdValueStr) - 1; //changes str to int, -1 to match the index
                 if (cmdValueInt > shoppingList.size()-1 || cmdValueInt < 0){
                     System.out.println("Incorrect item index");
                 }else{
